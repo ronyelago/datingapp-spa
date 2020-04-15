da9a0bdde70e81566a1af28e901f8884cf650d36
Ronye <ronye.rocha@gmail.com>
2020-04-15 02:03:35 -0300

    login amost done

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'https://localhost:44325/api/auth/';
+
+constructor(private client: HttpClient) { }
+
+  login(model: any) {
+    return this.client.post(this.baseUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+  }
+}
+
